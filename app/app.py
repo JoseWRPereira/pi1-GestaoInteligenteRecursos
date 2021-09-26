5c09f1e966b64a1eb0da0b14063cd485dc61e3da
JoseWRPereira <josewrpereira@gmail.com>
2021-09-26 00:44:25 -0300

    Passagem de parametro por um form html para o app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    str = request.args.get('fname')
+    curso = request.args.get('course')
+    if( str == "Jose"):
+        return "TesteVars: "+str+curso
+    else:
+        return render_template('index.html')
+
+
+@app.route("/consulta")
+def pages():
+    return render_template('/pages/consulta.html')
+
+
 
 app.run(host="0.0.0.0", port=5000)
