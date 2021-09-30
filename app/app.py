d0f419059ee681223bbefe4d3e7871d9f330f891
JoseWRPereira <josewrpereira@gmail.com>
2021-09-29 23:31:33 -0300

    Implementação das funções de inclusão, exclusão e edição de registro.

--- app/app.py
+++ app/app.py
@@ -73,6 +73,7 @@ def db_insert(sql,dados):
 
 
 def db_cmd(sql):
+    select = sql.count('SELECT') + sql.count('select')
     try:
         conn = mariadb.connect(
             host=db_conn_params["host"],
@@ -86,48 +87,51 @@ def db_cmd(sql):
         sys.exit(1)
     cursor = conn.cursor()
     cursor.execute(sql)
+    if select > 0:
+        lista = cursor.fetchall()
     conn.commit()
     conn.close()
-
+    if select > 0:
+        return lista
 
 
 
 @app.route("/")
 def index():
     sql = "SELECT * FROM users;"
-    cursor.execute(sql)
-    lista = cursor.fetchall()
+    lista = db_cmd(sql)
     return render_template('index.html', lista=lista)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        nome = request.form['fname']
-        curso = request.form['course']
+        nome = request.form['addname']
+        curso = request.form['addcourse']
         sql =  "INSERT INTO users (id, name, course) VALUES (?, ?, ?);"
-        data = [(4, str(nome), str(curso))]
+        data = [(5, str(nome), str(curso))]
         db_insert(sql,data)
         return redirect(url_for('index'))
     return render_template('add.html')
 
 @app.route("/delete/<row>", methods=['GET', 'POST'])
 def delete(row):
+    sql = "DELETE FROM users WHERE id={};".format(row)
+    db_cmd(sql)
+    return redirect(url_for('index'))
+
+@app.route("/edit/<id>", methods=['GET', 'POST'])
+def edit(id):
     if request.method == 'GET':
-        sql = "DELETE FROM users WHERE id={};".format(row)
+        sql = "SELECT * FROM users WHERE id={};".format(id)
+        lista = db_cmd(sql)
+        return render_template('edit.html', usuario=lista)
+    if request.method == 'POST':
+        nome = request.form['editname']
+        curso = request.form['editcourse']
+        sql = "UPDATE users SET name='{}', course='{}' WHERE id={};".format(nome,curso, id)
         db_cmd(sql)
-    return redirect(url_for('index'))
-
-
-@app.route("/edit/<row>", methods=['GET', 'POST'])
-def edit(row):
-    if request.method == 'GET':
-        sql = "SELECT * FROM users WHERE id={};".format(row)
-        cursor.execute(sql)
-        lista = cursor.fetchall()
-        return render_template('edit.html', lista=lista)
-    return redirect(url_for('index'))
-
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
